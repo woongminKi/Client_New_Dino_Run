91dcf49263f300a4769ca235715d7c1f1c359d29
기웅민 <kiwoongmin@giungmin-ui-MacBookPro.local>
2023-01-23 22:31:51 +0900

    Add: roomSlice and roomSaga

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,21 +1,26 @@
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import { all } from "redux-saga/effects";
+
 import auth from "../components/features/auth/authSlice";
 import user from "../components/features/user/userSlice";
-import { all } from "redux-saga/effects";
-import createSagaMiddleware from "redux-saga";
+import room from "../components/features/room/roomSlice";
+
 import authSaga from "../components/features/auth/authSaga";
 import userSaga from "../components/features/user/userSaga";
+import roomSaga from "../components/features/room/roomSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const reducer = combineReducers({
   auth,
   user,
+  room,
 });
 
 function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+  yield all([authSaga(), userSaga(), roomSaga()]);
 }
 
 const store = configureStore({
