f97956644e2144d9b2d7dc1e67c71e38ba72163b
woongminKi <dndals1991@gmail.com>
2023-02-21 22:22:00 +0900

    chore: test websocket url

--- src/modules/useSocket.jsx
+++ src/modules/useSocket.jsx
@@ -8,7 +8,7 @@ import {
   getPlayer2Video,
 } from "../components/features/game/gameSlice";
 
-export const socket = io.connect(process.env.REACT_APP_SERVER_URL, {
+export const socket = io.connect(process.env.REACT_APP_WEBSOCKET_SERVER_URL, {
   path: "/socket.io",
   transports: ["websocket"],
   cors: {
