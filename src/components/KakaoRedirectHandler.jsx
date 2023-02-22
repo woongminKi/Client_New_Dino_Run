0c1e95b6270103fc69e3e48c036f81069178c126
woongminKi <dndals1991@gmail.com>
2023-02-22 19:57:41 +0900

    fix: update url

--- src/components/KakaoRedirectHandler.jsx
+++ src/components/KakaoRedirectHandler.jsx
@@ -14,7 +14,8 @@ export default function KakaoRedirectHandler() {
   const loginStatus = useSelector((state) => state.auth.loginStatus);
 
   const restAPIKey = `${process.env.REACT_APP_CLIENT_API_ID}`;
-  const redirectURI = `${process.env.REACT_APP_DINO_URL}/oauth/kakao/callback`;
+  // const redirectURI = `${process.env.REACT_APP_DINO_URL}/oauth/kakao/callback`;
+  const redirectURI = `${process.env.REACT_APP_CLIENT_URL}/oauth/kakao/callback`;
   // const clientSecret = process.env.REACT_APP_CLIENT_SECRET_ID;
   const grantType = "authorization_code";
 
