4a4e9dcd3189aee4348da41976a0afeb7a5d7e9f
woongminKi <dndals1991@gmail.com>
2023-02-23 16:11:52 +0900

    check set cookies

--- src/utils/cookies.jsx
+++ src/utils/cookies.jsx
@@ -4,7 +4,9 @@ const cookies = new Cookies();
 
 export const setCookie = (name, value, option) => {
   console.log("setCookie call", name, value, option);
-  return cookies.set(name, value, { ...option });
+  const cookie = cookies.set(name, value, { ...option });
+  console.log("setCookie call22", { name, cookie });
+  return cookie;
 };
 
 export const getCookie = (name) => {
